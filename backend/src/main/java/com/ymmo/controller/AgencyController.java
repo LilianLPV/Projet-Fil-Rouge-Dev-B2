47ced113aa987b91f82aaaa7d99454b3071ab930
lilianlpv <lilian.lepiver@ynov.com>
2026-04-03 11:08:50 +0200

    Read.me backend + ajout routes api

--- backend/src/main/java/com/ymmo/controller/AgencyController.java
+++ backend/src/main/java/com/ymmo/controller/AgencyController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/agencies")
+@RequestMapping("/api/agencies")
 @CrossOrigin(origins = "*") // autorise les appels depuis le front-end
 public class AgencyController {
 
