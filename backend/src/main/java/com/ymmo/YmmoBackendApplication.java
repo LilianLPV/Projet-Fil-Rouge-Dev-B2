db56b437d533e5f9bd99765f4e944734074d22ab
lilianlpv <lilian.lepiver@ynov.com>
2026-04-03 09:24:09 +0200

    Mise à jour de l'initialisation du backend spring boot

---
+++ backend/src/main/java/com/ymmo/YmmoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ymmo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YmmoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YmmoBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/ymmo/ymmo_backend/YmmoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ymmo.ymmo_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YmmoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
